8d8777333d2707f3c3c4fa32f0f78a4dcf1ab0ac
Rob McCormack <56261775+robertmccormackbconline@users.noreply.github.com>
2026-08-26 14:58:47 +0100

    WRA-21 : Additional fields for warrant execution & sign off (#24)

---
+++ src/main/resources/db/migration/V1_5__add_responsible_officer_fields.sql
@@ -0,0 +1,3 @@
+ALTER TABLE public.warrant_risk_assessment ADD COLUMN responsible_officer_full_name varchar(200) NULL;
+ALTER TABLE public.warrant_risk_assessment ADD COLUMN responsible_officer_telephone_numer varchar(35) NULL;
+
