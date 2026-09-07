98c7268369ccc6dc4be767fcf914f83a39807f46
rmccormack <rmccormack@unilink.com>
2026-09-07 11:40:13 +0100

    WRA-30 / WRA-35 : Populating final PDF

--- src/main/kotlin/uk/gov/justice/digital/hmpps/warrantriskassessmentapi/service/WarrantRiskAssessmentService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/warrantriskassessmentapi/service/WarrantRiskAssessmentService.kt
@@ -58,7 +58,7 @@ class WarrantRiskAssessmentService(
   }
 
   fun getWarrantRiskAssessmentAsPdf(id: UUID, warrantRiskAssessment: WarrantRiskAssessment?, draft: Boolean): ByteArray? {
-    val html = pdfGenerationService.generateHtml(warrantRiskAssessment)
+    val html = pdfGenerationService.generateHtml(warrantRiskAssessment, id)
 
     var pdfBytes = pdfGenerationService.generatePdf(html)
 
