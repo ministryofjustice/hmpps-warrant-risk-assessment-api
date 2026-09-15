5788f71b7f576a28e374db524b60383632baea40
rmccormack <rmccormack@unilink.com>
2026-09-15 14:43:03 +0100

    WRA-30: Further header/footer changes for WRA pdf

--- src/main/kotlin/uk/gov/justice/digital/hmpps/warrantriskassessmentapi/service/PdfGenerationService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/warrantriskassessmentapi/service/PdfGenerationService.kt
@@ -44,6 +44,8 @@ class PdfGenerationService(
   fun generatePdf(html: String?): ByteArray? {
     val headers = HttpHeaders()
     headers.contentType = MediaType.MULTIPART_FORM_DATA
+    val header = templateEngine.process("header", Context())
+    val footer = templateEngine.process("footer", Context())
 
     val body = LinkedMultiValueMap<String, Any>()
     body.add(
@@ -53,6 +55,26 @@ class PdfGenerationService(
         HttpHeaders().apply {
           contentType = MediaType.TEXT_HTML
           setContentDispositionFormData("files", "index.html")
+        },
+      ),
+    )
+    body.add(
+      "files",
+      HttpEntity(
+        (header ?: "").toByteArray(StandardCharsets.UTF_8),
+        HttpHeaders().apply {
+          contentType = MediaType.TEXT_HTML
+          setContentDispositionFormData("files", "header.html")
+        },
+      ),
+    )
+    body.add(
+      "files",
+      HttpEntity(
+        (footer ?: "").toByteArray(StandardCharsets.UTF_8),
+        HttpHeaders().apply {
+          contentType = MediaType.TEXT_HTML
+          setContentDispositionFormData("files", "footer.html")
         },
       ),
     )
